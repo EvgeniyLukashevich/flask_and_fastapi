fcec886281fba0737723f8b14c609c85cd810ad9
EvgeniyLukashevich <humansyndr8me@gmail.com>
2023-09-15 09:09:48 +0800

    the 3rd seminar homework added

---
+++ homework/seminar3/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash
+from models import db, User
+from forms import RegistrationForm
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my_db.db"
+app.config["SECRET_KEY"] = '89f55340debe60e532e776da81276edfb0592d5a3bed02409dceedfdc07c4bd9'
+
+db.init_app(app)
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print("Db inited")
+
+
+@app.route("/", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        new_user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            password=hashed_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("success"))
+    return render_template("register.html", form=form)
+
+
+@app.route("/success")
+def success():
+    return "Регистрация прошла успешно!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ homework/seminar3/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
